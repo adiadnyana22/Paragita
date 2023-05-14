ac25b7411ba1bdb85a078aa14d7157e361703eec
Adi Adnyana <adiadnyana22@gmail.com>
2023-05-14 23:57:28 +0700

    Revision Commit (Background circle bug)

---
+++ resources/views/components/footer-index.blade.php
@@ -0,0 +1,48 @@
+<footer class="footer-index" id="contact">
+    <section class="footer-head">
+        <div class="container">
+            <div class="footer-head-container">
+                <div class="footer-head-section">
+                    <h2>Tentang kami</h2>
+                    <p>
+                        PSM UI Paragita adalah wadah bagi Mahasiswa UI yang sangat mengedepankan mutu dan kualitas dalam mengembangkan minat dan bakat di bidang berpaduan suara.
+                    </p>
+                </div>
+                <div class="footer-head-section">
+                    <p>
+                        Ada pertanyaan
+                        <span>admin@paragita.ui.ad</span>
+                    </p>
+                    <p>
+                        Telepon Kami
+                        <span>0852-1614-7761 (Risa)</span>
+                    </p>
+                </div>
+                <div class="footer-head-section">
+                    <h2>Alamat kami</h2>
+                    <p>
+                        Gedung Pusat Kegiatan Mahasiswa Baru (PUSGIWA UI) Jl. Prof. Dr. Fuad Hassan, Kukusan, Beji, Depok, Indonesia
+                    </p>
+                    <a href="#">Lihat Peta</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container">
+        <div class="footer-container">
+            <div class="footer-profile">
+                <img src="{{ asset('assets/images/logoWhite.png') }}" alt="Logo Paragita">
+                <div class="footer-text">
+                    <p>© 2015 - 2022 Paduan Suara Mahasiswa Universitas Indonesia.<br>All Rights Reserved.</p>
+                </div>
+            </div>
+            <div class="footer-link">
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+                <a href="#"><i class='bx bxl-instagram' ></i></a>
+                <a href="#"><i class='bx bxl-twitter' ></i></a>
+                <a href="#"><i class='bx bxl-whatsapp' ></i></a>
+                <a href="#"><i class='bx bxl-youtube' ></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,39 +1,50 @@
-@extends('components.template')
+@extends('components.template-index')
 
 @section('title', 'Paragita')
 
 @section('pageHeadExtention')
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}">
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 @endsection
 
 @section('content')
     <section class="bg-banner">
-        <div class="container">
-            <div class="banner-head">
-                <div class="banner-title">
-                    <p>We are also known as ‘PSM UI Paragita’ or ‘Paragita Choir’</p>
-                    <h1>Paduan Suara Mahasiswa<br>Universitas Indonesia<span>Paragita</span></h1>
-                    <img class="banner-vector" src="{{ asset('assets/images/vectorBanner.png') }}" alt="Vector Banner">
-                </div>
-                <div class="banner-link">
-                    <a href="#about">
-                        <p>Pelajari lebih tentang PSM UI Paragita</p>
-                        <span><i class='bx bx-down-arrow-circle' ></i></span>
-                    </a>
+        <div class="main-carousel" data-flickity='{ "cellAlign": "left", "prevNextButtons": false, "autoPlay": true, "wrapAround": true }'>
+            <div class="carousel">
+                <img src="{{ asset('assets/images/banner1.png') }}" alt="Banner Image">
+            </div>
+            <div class="carousel">
+                <img src="{{ asset('assets/images/banner1.png') }}" alt="Banner Image">
+            </div>
+            <div class="carousel">
+                <img src="{{ asset('assets/images/banner1.png') }}" alt="Banner Image">
+            </div>
+        </div>
+        <div class="banner-text">
+            <div class="container">
+                <div class="banner-head">
+                    <div class="banner-title">
+                        <p>Kita juga dikenal sebagai ‘PSM UI Paragita’ atau ‘Paragita Choir’</p>
+                        <h1>Paduan Suara Mahasiswa<br>Universitas Indonesia</h1>
+                        <img class="banner-logo" src="{{ asset('assets/images/logoWhite.png') }}" alt="Logo Banner">
+                    </div>
                 </div>
             </div>
-            <div class="banner-about" id="about">
-                <div class="about-card">
-                    <div class="about-image">
-                        <img src="{{ asset('assets/images/logoWhite.png') }}" alt="Logo Paragita">
-                    </div>
-                    <div class="about-text">
-                        <h2>Tentang Kami,</h2>
-                        <p>
-                            PSM UI Paragita adalah wadah bagi Mahasiswa UI yang sangat mengedepankan mutu dan kualitas dalam mengembangkan minat dan bakat di bidang berpaduan suara. Dengan fokus pada pengembangan bakat sumber daya yang dimilikinya, PSM UI Paragita telah meraih prestasi dan pencapaian yang membanggakan.
-                        </p>
-                        <a href="/coach">Learn more <i class='bx bx-right-arrow-alt'></i></a>
-                    </div>
+        </div>
+    </section>
+    <section class="bg-about">
+        <div class="banner-about" id="about">
+            <div class="about-card">
+                <div class="about-image">
+                    <img src="{{ asset('assets/images/logoWhite.png') }}" alt="Logo Paragita">
+                </div>
+                <div class="about-text">
+                    <h2>Tentang Kami,</h2>
+                    <p>
+                        PSM UI Paragita adalah wadah bagi Mahasiswa UI yang sangat mengedepankan mutu dan kualitas dalam mengembangkan minat dan bakat di bidang berpaduan suara. Dengan fokus pada pengembangan bakat sumber daya yang dimilikinya, PSM UI Paragita telah meraih prestasi dan pencapaian yang membanggakan.
+                    </p>
+                    <a href="/coach">Learn more <i class='bx bx-right-arrow-alt'></i></a>
                 </div>
             </div>
         </div>
@@ -43,7 +54,7 @@
             <div class="achievement-head">
                 <div class="achievement-title">
                     <span>PSM UI Paragita’s</span>
-                    <h2>Achievements</h2>
+                    <h2>Penghargaan</h2>
                 </div>
                 <div class="achievement-btn">
                     <a href="/achievement">Pelajari prestasi Paragita <i class='bx bx-right-arrow-alt'></i></a>
@@ -82,73 +93,79 @@
             </div>
         </div>
     </section>
-    <section class="bg-news" id="news">
-        <div class="container">
-            <div class="news-head">
-                <div class="news-title">
-                    <span>This is our recent activiy,</span>
-                    <h2>News</h2>
-                </div>
-                <div class="news-btn">
-                    <a href="#">Read more news <i class='bx bx-right-arrow-alt'></i></a>
-                </div>
-            </div>
-            <div class="news-list">
-                <div class="news-card">
-                    <div class="news-image">
-                        <img src="{{ asset('assets/images/news.png') }}" alt="News">
-                        <div class="backdrop"></div>
-                    </div>
-                    <div class="news-text">
-                        <p>06 March, 2023</p>
-                        <h3>The Resonanz Children’s Choir Raih Juara European Grand Prix in Choral Singing</h3>
-                    </div>
-                </div>
-                <div class="news-card">
-                    <div class="news-image">
-                        <img src="{{ asset('assets/images/news.png') }}" alt="News">
-                        <div class="backdrop"></div>
-                    </div>
-                    <div class="news-text">
-                        <p>06 March, 2023</p>
-                        <h3>The Resonanz Children’s Choir Raih Juara European Grand Prix in Choral Singing</h3>
-                    </div>
-                </div>
-                <div class="news-card">
-                    <div class="news-image">
-                        <img src="{{ asset('assets/images/news.png') }}" alt="News">
-                        <div class="backdrop"></div>
-                    </div>
-                    <div class="news-text">
-                        <p>06 March, 2023</p>
-                        <h3>The Resonanz Children’s Choir Raih Juara European Grand Prix in Choral Singing</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="bg-gallery" id="gallery">
-        <div class="gallery-title">
+    <section class="bg-news-gallery">
+        <section class="bg-news" id="news">
             <div class="container">
-                <span>This is our recent activiy,</span>
-                <h2>Galleries</h2>
+                <div class="news-head">
+                    <div class="news-title">
+                        <span>This is our recent activiy,</span>
+                        <h2>Berita</h2>
+                    </div>
+                    <div class="news-btn">
+                        <a href="#">Read more news <i class='bx bx-right-arrow-alt'></i></a>
+                    </div>
+                </div>
+                <div class="news-list">
+                    <a class="news-card" href="#">
+                        <div class="news-image">
+                            <img src="{{ asset('assets/images/news.png') }}" alt="News">
+                            <div class="backdrop"></div>
+                        </div>
+                        <div class="news-text">
+                            <p>06 March, 2023</p>
+                            <h3>The Resonanz Children’s Choir Raih Juara European Grand Prix in Choral Singing</h3>
+                        </div>
+                    </a>
+                    <a class="news-card" href="#">
+                        <div class="news-image">
+                            <img src="{{ asset('assets/images/news.png') }}" alt="News">
+                            <div class="backdrop"></div>
+                        </div>
+                        <div class="news-text">
+                            <p>06 March, 2023</p>
+                            <h3>The Resonanz Children’s Choir Raih Juara European Grand Prix in Choral Singing</h3>
+                        </div>
+                    </a>
+                    <a class="news-card" href="#">
+                        <div class="news-image">
+                            <img src="{{ asset('assets/images/news.png') }}" alt="News">
+                            <div class="backdrop"></div>
+                        </div>
+                        <div class="news-text">
+                            <p>06 March, 2023</p>
+                            <h3>The Resonanz Children’s Choir Raih Juara European Grand Prix in Choral Singing</h3>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </div>
-        <div class="gallery-body">
-            <div class="gallery-list">
-                <img src="{{ asset('assets/images/news11.png') }}" alt="Gallery">
-                <img src="{{ asset('assets/images/news12.png') }}" alt="Gallery">
-                <img src="{{ asset('assets/images/news13.png') }}" alt="Gallery">
-                <img src="{{ asset('assets/images/news14.png') }}" alt="Gallery">
-                <img src="{{ asset('assets/images/news15.png') }}" alt="Gallery">
+        </section>
+        <section class="bg-gallery" id="gallery">
+            <div class="gallery-title">
+                <div class="container">
+                    <span>This is our recent activiy,</span>
+                    <h2>Galeri Foto</h2>
+                </div>
             </div>
-            <div class="gallery-list">
-                <img src="{{ asset('assets/images/news21.png') }}" alt="Gallery">
-                <img src="{{ asset('assets/images/news22.png') }}" alt="Gallery">
-                <img src="{{ asset('assets/images/news23.png') }}" alt="Gallery">
-                <img src="{{ asset('assets/images/news24.png') }}" alt="Gallery">
-                <img src="{{ asset('assets/images/news25.png') }}" alt="Gallery">
+            <div class="gallery-body">
+                <div class="gallery-list">
+                    <img src="{{ asset('assets/images/news11.png') }}" alt="Gallery">
+                    <img src="{{ asset('assets/images/news12.png') }}" alt="Gallery">
+                    <img src="{{ asset('assets/images/news13.png') }}" alt="Gallery">
+                    <img src="{{ asset('assets/images/news14.png') }}" alt="Gallery">
+                    <img src="{{ asset('assets/images/news15.png') }}" alt="Gallery">
+                </div>
+                <div class="gallery-list">
+                    <img src="{{ asset('assets/images/news21.png') }}" alt="Gallery">
+                    <img src="{{ asset('assets/images/news22.png') }}" alt="Gallery">
+                    <img src="{{ asset('assets/images/news23.png') }}" alt="Gallery">
+                    <img src="{{ asset('assets/images/news24.png') }}" alt="Gallery">
+                    <img src="{{ asset('assets/images/news25.png') }}" alt="Gallery">
+                </div>
             </div>
-        </div>
+        </section>
+{{--        Circle Ngebug kalo klik gallery di navbar--}}
+{{--        <div class="news-gallery-circle1"></div>--}}
+{{--        <div class="news-gallery-circle2"></div>--}}
+{{--        <div class="news-gallery-circle3"></div>--}}
     </section>
 @endsection
