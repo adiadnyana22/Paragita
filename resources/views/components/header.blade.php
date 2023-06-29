a06df7ec1bef3f5d589ac0c1cd4adf3ff2271d5d
Adi Adnyana <adiadnyana22@gmail.com>
2023-06-30 00:29:31 +0700

    Don bang

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav>
             <div class="head-logo"><a href="{{ route('index') }}"><img src="{{ asset('assets/images/logoWhite.png') }}" alt="Logo Paragita"></a></div>
-            <ul>
+            <ul id="nav-desktop">
                 <li><a href="{{ route('index') }}">Beranda</a></li>
                 <li class="nav-dropdown">
                     <a href="#">Tentang Kami <i class='bx bx-chevron-down' ></i></a>
@@ -15,6 +15,9 @@
                 <li><a href="{{ route('newsList') }}">Berita</a></li>
                 <li><a href="{{ route('gallery') }}">Galeri</a></li>
                 <li><a href="https://wa.me/081246868369" target="_blank" class="btn-highlight">Hubungi Kami</a></li>
+            </ul>
+            <ul id="nav-mobile">
+                <li><a href="#" id="burger-btn"><i class='bx bx-menu-alt-right' ></i></a></li>
             </ul>
         </nav>
     </div>
--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -17,5 +17,52 @@
     @include('components.header')
     @yield('content')
     @include('components.footer')
+    <div class="nav-mobile" id="nav-mobile-menu">
+        <ul>
+            <li><a href="{{ route('index') }}">Beranda</a></li>
+            <li>
+                <a href="#" id="nav-mobile-dropdown-btn">Tentang Kami <i class='bx bx-chevron-down' ></i></a>
+                <div class="nav-mobile-dropdown" id="nav-mobile-dropdown">
+                    <a href="{{ route('about') }}">Paragita UI</a>
+                    <a href="{{ route('coach') }}">Pelatih</a>
+                </div>
+            </li>
+            <li><a href="{{ route('achievement') }}">Penghargaan</a></li>
+            <li><a href="{{ route('newsList') }}">Berita</a></li>
+            <li><a href="{{ route('gallery') }}">Galeri</a></li>
+        </ul>
+        <a href="#" class="nav-close-btn" id="close-btn"><i class='bx bx-x'></i></a>
+    </div>
+
+    <script>
+        let closeBtn = document.querySelector('#close-btn');
+        let burgerBtn = document.querySelector('#burger-btn');
+        let mobileNav = document.querySelector('#nav-mobile-menu');
+
+        closeBtn.addEventListener('click', () => {
+            mobileNav.style.width = 0;
+        });
+
+        burgerBtn.addEventListener('click', () => {
+            mobileNav.style.width = '100vw';
+        });
+    </script>
+
+    <script>
+        let mobileDropdownBtn = document.querySelector('#nav-mobile-dropdown-btn');
+        let mobileDropdownPanel = document.querySelector('#nav-mobile-dropdown');
+
+        mobileDropdownBtn.addEventListener('click', () => {
+            if(mobileDropdownPanel.classList.contains('active')){
+                mobileDropdownPanel.style.height = '0';
+                mobileDropdownPanel.style.marginTop = '0';
+                mobileDropdownPanel.classList.remove('active');
+            } else {
+                mobileDropdownPanel.style.height = '125px';
+                mobileDropdownPanel.style.marginTop = '1.5rem';
+                mobileDropdownPanel.classList.add('active');
+            }
+        });
+    </script>
 </body>
 </html>
